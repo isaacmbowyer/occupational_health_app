f2d27b8ab418dd16b45a46f6be2569d08b6589c6
Isaac Bowyer <isaacbowyer@googlemail.com>
2024-03-25 16:25:47 +0000

    fix: removed assets

--- src/components/navigation/NavigationContainer/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NavigationContainer as NavContainer } from "@react-navigation/native";
-
-export const NavigationContainer = () => {
-  return (
-    <NavContainer>
-      <></>
-    </NavContainer>
-  );
-};

c3c68cc1ca2921af7fd39813f4ed94e101fce66a
Isaac Bowyer <isaacbowyer@googlemail.com>
2024-03-27 15:52:00 +0000

    feat: added dropdown hooks

---
+++ src/utils/createDropdownOptions/index.ts
@@ -0,0 +1,14 @@
+import { IOption } from "../../entities/IOption";
+
+export const createDropdownOptions: ICreateDropdownOptions = (list) => {
+  return list.map((item, index) => {
+    return {
+      id: `${index + 1}`,
+      name: item,
+    };
+  });
+};
+
+interface ICreateDropdownOptions {
+  (list: string[]): IOption[];
+}

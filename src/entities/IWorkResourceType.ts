50e0ad903580ef894b8527f9730f782b2d617905
Isaac Bowyer <isaacbowyer@googlemail.com>
2024-04-11 04:18:40 +0100

    feat: added work resources screens

---
+++ src/entities/IWorkResourceType.ts
@@ -0,0 +1,7 @@
+export type IWorkResourceType =
+  | "Employee Rights"
+  | "Employer Rights"
+  | "Workplace Adjustments"
+  | "Disibility Entitlements"
+  | "Mental Health Support"
+  | "Financial Support";

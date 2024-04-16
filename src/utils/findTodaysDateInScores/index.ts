e7a7de1ce5dc619cbe01ccc16d7fa67edddb56c5
Isaac Bowyer <isaacbowyer@googlemail.com>
2024-04-16 12:59:24 +0100

    feat: addec comment

--- src/utils/findTodaysDateInScores/index.ts
+++ src/utils/findTodaysDateInScores/index.ts
@@ -2,7 +2,7 @@ import { ISymptomScore } from "../../entities/ISymptomScore";
 import { formatDate } from "../formatDate";
 
 export const findTodaysDateInScores: IFindTodaysDateInScoresUtil = (scores) => {
-  const todayDate = new Date();
+  const todayDate = new Date(); // date
 
   const foundScore = scores?.find(
     (score) => formatDate(score.createdAt) === formatDate(todayDate)
